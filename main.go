cc16acea858a1f7062850bcd75ca0630d82c8ead
Ilya Erokhin <sniffsla@gmail.com>
2023-11-21 22:26:40 +0300

    +logrus

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"github.com/imconfly/imconfly_go/cli"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"os"
 )
 
 func main() {
+	log.SetLevel(log.TraceLevel)
 	if err := cli.App.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
--- server/run_server.go
+++ server/run_server.go
@@ -1,18 +1,19 @@
 package server
 
 import (
-	"fmt"
 	"github.com/imconfly/imconfly_go/configuration"
 	"github.com/imconfly/imconfly_go/core/internal_workers"
 	"github.com/imconfly/imconfly_go/core/queue"
 	"github.com/imconfly/imconfly_go/core/resolver"
 	"github.com/imconfly/imconfly_go/core/transforms_conf"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
 func RunServer(conf *configuration.Conf, trConf *transforms_conf.Conf) error {
 	var originQ *queue.Queue
 	if trConf.HaveNonLocalOrigins() {
+		log.Debug("Non-local origins found.")
 		originQ = queue.NewQueue()
 		defer originQ.Close()
 		for i := 0; i < conf.TransformConcurrency; i++ {
@@ -30,6 +31,6 @@ func RunServer(conf *configuration.Conf, trConf *transforms_conf.Conf) error {
 		Resolver: resolver.NewResolver(transformsQ, trConf, conf.DataDir, conf.TmpDir),
 	}
 
-	fmt.Printf("Server is listening on %s\n", conf.ServerAddr)
+	log.Infof("Server is listening on %s\n", conf.ServerAddr)
 	return http.ListenAndServe(conf.ServerAddr, handler)
 }
